691e15508b540b4948e6b9bceadbafe6a6225035
nika <nika3141535@gmail.com>
2024-07-21 03:22:42 +0400

    add deleteOne middleware

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,7 +2,7 @@ const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getOne = (Model) => catchAsync(async(req,res,next)=>{
-    let doc = await Model.findById(req.params.id)
+    const doc = await Model.findById(req.params.id)
 
     if(!doc){
         return next(new AppError('no document found with this id ',404))
@@ -16,7 +16,20 @@ exports.getOne = (Model) => catchAsync(async(req,res,next)=>{
     })
 })
 
+exports.getAll = (Model) => catchAsync(async(req,res,next) => {
+    
+    const doc = await Model.find()
 
+    if(!doc || !doc.length == 0)return next(new AppError('no document found',404))
+
+    res.status(200).json({
+        status:'success',
+        result:doc.length,
+        data:{
+            doc
+        }
+    })
+})
 
 
 exports.createOne = (Model) => catchAsync(async(req,res,next) => {
@@ -29,4 +42,12 @@ exports.createOne = (Model) => catchAsync(async(req,res,next) => {
             doc
         }
     })
+})
+
+exports.deleteOne = Model => catchAsync(async(req,res,next)=>{
+    const doc = await Model.findByIdAndDelete(req.params.id)
+
+    if(!doc){
+        return next(new AppError(`no document found with that id`, 404))
+    }
 })
