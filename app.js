5a3e78c50fd968dadc94e2830e13fbdec2f4b6eb
nika <nika3141535@gmail.com>
2024-07-21 02:07:12 +0400

    security configurations

--- app.js
+++ app.js
@@ -2,9 +2,51 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
-const test = require('./routes/test')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+const compression = require('compression')
+
+
+const test = require('./routes/test');
+const { mongo } = require("mongoose");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(helmet())
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'))
+}
+
+const limiter = rateLimit({
+    max:100,
+    windowMs:60*60*1000,
+    message:'Too many request from this IP, please try again in an hour'
+})
+
+app.use('/api',limiter)
+
+app.use(express.json({limit:'10kb'}))
+
+app.use(mongoSanitize())
+
+app.use(xss())
+
+// app.use(hpp({
+
+// }))
+
+app.use(compression())
+
+app.use((req, res, next) => {
+    res.requestTime = new Date().toISOString();
+  
+    next();
+  });
+  
 
 app.use('/',test)
 
