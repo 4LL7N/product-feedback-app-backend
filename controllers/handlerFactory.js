787cdbcfb8b8629051fe236e837af2d5fee310fa
nika <nika3141535@gmail.com>
2024-07-21 03:26:36 +0400

    add updateOne middleware

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -51,3 +51,19 @@ exports.deleteOne = Model => catchAsync(async(req,res,next)=>{
         return next(new AppError(`no document found with that id`, 404))
     }
 })
+
+exports.updateOne = Model => catchAsync(async(req,res,next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    })
+
+    if(!doc)return next(new AppError('no document found with that id',404))
+
+    res.status(200).json({
+        status:'success',
+        data:{
+            doc
+        }
+    })
+})
