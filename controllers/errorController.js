49cd935a54df35a07b7e35889452d1bb6e2631f4
nika <nika3141535@gmail.com>
2024-07-21 11:42:15 +0400

    add Validation and develoment Error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,29 @@
-const AppError = require("../utils/appError")
+const AppError = require("../utils/appError");
 
-const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}: ${err.value}. `
-    return new AppError(message,400)
-}
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}. `;
+  return new AppError(message, 400);
+};
 
-const handleDuplicateFieldsDB = err => {
-    const value = Object.values(err.keyValue)[0]
-    const message = `Duplicate field value: ${value} Please use another value`
+const handleDuplicateFieldsDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+  const message = `Duplicate field value: ${value} Please use another value`;
 
-    return new AppError(message,400)
-}
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = ` Invalid input data. ${errors.join(". ")}`;
+
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, req, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
