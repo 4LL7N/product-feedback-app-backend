e1f171d503fd460f67a24733759d75e3e68061d5
nika <nika3141535@gmail.com>
2024-07-21 11:51:15 +0400

    add module which combines all error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,4 @@
+const { model } = require("mongoose");
 const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
@@ -28,18 +29,41 @@ const sendErrorDev = (err, req, res) => {
   });
 };
 
-const sendErrorProd = (err,req,res) => {
-    if(err.isOperational){
-        return res.status(err.statusCode).json({
-            status:err.status,
-            message:err.message
-        })
-    }
+const sendErrorProd = (err, req, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
 
-    console.error(err);
+  console.error(err);
 
-    return res.status(500).json({
-        status:'error',
-        message:'Something went very wrong'
-    })
-}
+  return res.status(500).json({
+    status: "error",
+    message: "Something went very wrong",
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV == "development") {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV == "production") {
+    let error = err;
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    
+    if (error.code == 11000) error = handleDuplicateFieldsDB(error);
+
+    if (error.name == "ValidationError") error = handleValidationErrorDB(error);
+
+    if (error.name == "JsonWebTokenError") error = handleJWTError(error);
+
+    if (error.name == "TokenExpiredError") error = handleJWTExpiredError(error);
+
+    sendErrorProd(error, req, res);
+  }
+};
